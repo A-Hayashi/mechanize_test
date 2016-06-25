480ca6b76fe85a188166dd94fb37499f72562d4a
dracen <a.hayashi0113@gmail.com>
2016-06-26 03:13:11 +0900

    画像アップローダへの画像アップロード機能と削除機能追加

--- test.rb
+++ test.rb
@@ -1,22 +1,65 @@
 # -*- encoding: Shift_JIS -*-
 require 'rubygems'
 require 'mechanize'
+require 'pp'
 
-agent = Mechanize.new
-agent.keep_alive = false
-agent.max_history = 1
-agent.open_timeout = 60
-agent.read_timeout = 180
-agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+module Gazo
+  module_function
+  def upload
+    agent = Mechanize.new
+    agent.keep_alive = false
+    agent.max_history = 1
+    agent.open_timeout = 60
+    agent.read_timeout = 180
+    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-targetASIN = "4774158968" # ASINコード
-targetURI = "http://amazon.jp/dp/" + targetASIN.chomp # URLの編集
-puts targetURI
+    targetURI = "http://s1.gazo.cc/" # URLの編集
+    puts targetURI
 
-agent.get(targetURI) #webPageの取得
+    agent.get(targetURI) do |page| #webPageの取得
 
-puts agent.page.search('title').inner_text # タイトル
-sleep(1)
-src = agent.page.search('//*[@id="imgBlkFront"]').at('img')['src'] # 商品画像
-puts src
-agent.get(src).save_as("./img/" + targetASIN.chomp + ".jpg") # 保存する
+      page.form_with(name: 'Form') do |form|
+        form.file_upload.file_name = './img/4774158968.jpg'
+        form.field_with(:name => 'pass').value = "2357"
+        form.submit
+      end
+
+    end
+
+    puts("done")
+  end
+
+  def delete
+    @file_no=[]
+    agent = Mechanize.new
+    agent.keep_alive = false
+    agent.max_history = 1
+    agent.open_timeout = 60
+    agent.read_timeout = 180
+    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    targetURI = "http://s1.gazo.cc/" # URLの編集
+    puts targetURI
+
+    agent.get(targetURI) do |page| #webPageの取得
+      page.search("//tr/td[@class='file']/a").each do |file|
+        @file_no << file.inner_text.gsub(/(\d+)\.(.+)/,'\1')
+      end
+
+      page.form_with(name: 'Del') do |form|
+        @file_no.each do |no|
+          form.field_with(name: 'delno').value = no
+          form.field_with(name: 'delpass').value = "2357"
+          form.submit
+          sleep(0.5)
+          puts no
+        end
+
+      end
+
+    end
+    puts "done"
+  end
+end
+
+Gazo.delete
